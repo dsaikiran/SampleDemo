0bc0d2075e58be8effbf283fd6d71604fbd25e3a
saikirand <saikirand@vedams.com>
2016-12-15 19:39:38 +0530

    added data.txt file, reading data from text file, added test cases and loggers

--- src/main/java/com/techolution/knapsack/Solution.java
+++ src/main/java/com/techolution/knapsack/Solution.java
@@ -1,42 +1,150 @@
 package com.techolution.knapsack;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author saikiran
+ *
+ */
+
+/**
+ * This class reads content from data.txt file from resource location.
+ * It returns maximum satisfaction using knapsack algorithm.
+ *
+ */
 @Component
-class Solution{
+class Solution {
+	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(Solution.class);
+	
+	// reading data file name from application.properties
+    private static String fileName;
     
-     public static int knapsack(int val[], int wt[], int W) {
-         int N = wt.length; // Get the total number of items. Could be wt.length or val.length. Doesn't matter
-         int[][] V = new int[N + 1][W + 1]; //Create a matrix. Items are in rows and weight at in columns +1 on each side
-         //What if the knapsack's capacity is 0 - Set all columns at row 0 to be 0
-         for (int col = 0; col <= W; col++) {
-             V[0][col] = 0;
-         }
-         //What if there are no items at home.  Fill the first row with 0
-         for (int row = 0; row <= N; row++) {
-             V[row][0] = 0;
-         }
-         for (int item=1;item<=N;item++){
-             //Let's fill the values row by row
-             for (int weight=1;weight<=W;weight++){
-                 //Is the current items weight less than or equal to running weight
-                 if (wt[item-1]<=weight){
-                     //Given a weight, check if the value of the current item + value of the item that we could afford with the remaining weight
-                     //is greater than the value without the current item itself
-                     V[item][weight]=Math.max (val[item-1]+V[item-1][weight-wt[item-1]], V[item-1][weight]);
-                 }
-                 else {
-                     //If the current item's weight is more than the running weight, just carry forward the value without the current item
-                     V[item][weight]=V[item-1][weight];
-                 }
-             }
-         }
-         //Printing the matrix
-         for (int[] rows : V) {
-             for (int col : rows) {
-                // System.out.format("%5d", col);
-             }
-         }
-          return V[N][W];
+ 	@Autowired
+    public void MyBean(@Value("${input.filename}") String fileName) {
+        this.fileName = fileName;
      }
- }
+	
+	static int[] val = new int[100];
+	static int[] wt = new int[100];
+	static int W = 0;
+	static int N = 0;// Get the total number of items. Could be wt.length or
+					 // val.length. Doesn't matter
+
+	public static int knapsack() throws IOException {
+		
+		// read content from data.txt file
+		readContentFromFile();
+		
+		LOGGER.info("Applying knapsnack on data...");
+ 		int[][] V = new int[N + 1][W + 1]; // Create a matrix. Items are in rows
+											// and weight at in columns +1 on
+											// each side
+		// What if the knapsack's capacity is 0 - Set all columns at row 0 to be
+		// 0
+		for (int col = 0; col <= W; col++) {
+			V[0][col] = 0;
+		}
+		// What if there are no items at home. Fill the first row with 0
+		for (int row = 0; row <= N; row++) {
+			V[row][0] = 0;
+		}
+		for (int item = 1; item <= N; item++) {
+			// Let's fill the values row by row
+			for (int weight = 1; weight <= W; weight++) {
+				// Is the current items weight less than or equal to running
+				// weight
+				if (wt[item - 1] <= weight) {
+					// Given a weight, check if the value of the current item +
+					// value of the item that we could afford with the remaining
+					// weight
+					// is greater than the value without the current item itself
+					V[item][weight] = Math.max(val[item - 1]
+							+ V[item - 1][weight - wt[item - 1]],
+							V[item - 1][weight]);
+				} else {
+					// If the current item's weight is more than the running
+					// weight, just carry forward the value without the current
+					// item
+					V[item][weight] = V[item - 1][weight];
+				}
+			}
+		}
+		// Printing the matrix
+		/*for (int[] rows : V) {
+			for (int col : rows) {
+				 System.out.format("%5d", col);
+			}
+		}*/
+ 		return V[N][W];
+	}
+
+	/**
+	 * This method reads data from file and parses it.
+	 * @return 
+	 * 
+	 */
+	public static String readContentFromFile() throws NumberFormatException
+			 {
+		LOGGER.info("Reading content from file...");
+		int counter = 0;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		String line;
+		 
+		// logic to read content from file
+		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			LOGGER.error("File not found, please check file is located in resources folder...");
+			e.printStackTrace();
+		}
+		try {
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(" ", 2);
+				if (parts.length >= 2) {
+					// get maximum time and number of items
+					if (parts[0].equals("10000") & parts[1].equals("100")) {
+						W = (int) Integer.parseInt(parts[0]);
+						N = (int) Integer.parseInt(parts[1]);
+					} // get satisfaction, time and add to map
+					else {
+						map.put((int) Integer.parseInt(parts[0]),
+								(int) Integer.parseInt(parts[1]));
+					}
+				} else {
+					LOGGER.warn("ignoring line: " + line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// loop map elements and add to int array.
+		for (int key : map.keySet()) {
+			val[counter] = key;
+			wt[counter] = map.get(key);
+			counter++;
+		}
+		try {
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info("File read successfully...");
+		return "success";
+	}
+}
--- src/test/java/com/techolution/knapsack/SolutionTests.java
+++ src/test/java/com/techolution/knapsack/SolutionTests.java
@@ -3,6 +3,10 @@ package com.techolution.knapsack;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -48,19 +52,28 @@ public class SolutionTests {
 	
 	/**
 	 * should return 2493893
+	 * @throws IOException 
 	 * 
 	 */
 	@Test
-	public void testKnapsackExpectedToReturn2493893(){
-		int val[]= {16808,50074,8931,27545,77924,64441,84493,7988,82328,78841,44304,17710,29561,93100,51817,99098,13513,23811,80980,36580,11968,1394,25486,25229,40195,35002,16709,15669,88125,9531,27723,28550,97802,40978,8229,60299,28636,23866,39064,39426,24116,75630,46518,30106,19452,82189,99506,6753,36717,54439,51502,83872,11138,53178,22295,21610,59746,53636,98143,27969,261,41595,16396,19114,71007,97943,42083,30768,85696,73672,48591,14739,31617,55641,37336,97973,49096,83455,12290,48906,36124,45814,35239,96221,12367,25227,41364,7845,36551,8624,97386,95273,99248,13497,40624,28145,35736,61626,46043,54680};
- 		int wt[]= {250,659,273,879,710,166,43,504,730,613,170,158,934,279,336,827,268,634,150,822,673,337,746,92,358,154,945,491,197,904,667,25,854,409,934,982,14,815,537,670,95,502,196,405,299,124,883,567,338,145,898,829,359,398,905,232,176,299,400,413,558,9,969,531,963,366,853,822,713,902,832,58,791,680,7,99,320,224,761,127,507,771,95,845,535,395,739,591,160,948,218,540,386,886,421,969,916,535,12,153};
- 		int W = 10000;
- 		 
-		int result = Solution.knapsack(val, wt, W);
+	public void testKnapsackExpectedToReturn2493893() throws IOException{
+		int result = Solution.knapsack();
 		assertNotNull(result);
 		assertEquals(2493893, result);
  	}
-
+	
+	 
+	/**
+	 * readContentFromFileShouldReturnEmpty
+	 * @throws IOException 
+	 * 
+	 */
+	@Test
+	public void readContentFromFileShouldReturnEmpty() throws IOException{
+  		assertEquals("success",  Solution.readContentFromFile());
+ 	}
+	
+	
 	/**
 	 * after test
 	 */
